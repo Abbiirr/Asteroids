83276710d131fdd79399702212d265b8a9e2a23e
Abir <abir.hossain2107@gmail.com>
2023-01-20 19:41:05 +0600

    Created ProjectileGeneratorTest

---
+++ src/test/ProjectileGeneratorTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import game.Asteroid;
+import game.ProjectileGenerator;
+import javafx.scene.layout.Pane;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectileGeneratorTest {
+
+    @Test
+    @DisplayName("Generate asteroids")
+    void generateAsteroids() {
+        ArrayList<Asteroid> asteroids = new ArrayList<>();
+        Pane pane = new Pane();
+        ProjectileGenerator projectileGenerator = new ProjectileGenerator();
+        projectileGenerator.generateAsteroids(asteroids);
+        assertNotNull(asteroids);
+    }
+}
