82d2052afa129d194e277ba4893d6d1ccaabde9d
Abir <abir.hossain2107@gmail.com>
2023-01-20 19:31:08 +0600

    Created projectile generator

---
+++ src/game/ProjectileGenerator.java
@@ -0,0 +1,29 @@
+package game;
+
+import javafx.scene.layout.Pane;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ProjectileGenerator {
+     public  ProjectileGenerator() {
+
+    };
+
+    public void generateAsteroids(ArrayList<Asteroid> asteroids){
+        Random rnd = new Random();
+        int numberAsteroids = rnd.nextInt(8) + 3;
+        for (int i = 0; i < numberAsteroids; i++) {
+            Asteroid asteroid = new Asteroid();
+            asteroids.add(asteroid);
+        }
+    }
+    public void renderAsteroids(ArrayList<Asteroid> list, Pane pane){
+        list.stream().forEach(projectile -> pane.getChildren().add(projectile.getShapeHeavenlyBody()));
+    }
+    public void makeAndRender(Pane gameWindow, ArrayList<Asteroid> asteroids){
+//
+        generateAsteroids(asteroids);
+        renderAsteroids(asteroids,gameWindow);
+    }
+}
